5fffeafd31ef33931767cc6d0b9ed3dcd5647f4a
zhuhuihui <jackjboss@gmail.com>
2017-05-13 18:18:44 +0800

    初始化递归创建文件夹

--- src/main/java/com/gochinatv/cdn/api/service/impl/LocalCacheImpl.java
+++ src/main/java/com/gochinatv/cdn/api/service/impl/LocalCacheImpl.java
@@ -1,5 +1,7 @@
 package com.gochinatv.cdn.api.service.impl;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.ExecutionException;
@@ -10,27 +12,63 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
 public class LocalCacheImpl {
-   
+
 	private Path importPath;
 	private LoadingCache<Integer, String> keyValueCache;
-	
+	private String export;
+	private String exportHour;
+	private String exportWeek;
+
 	public void setImportPath(String importPath) {
 		this.importPath = Paths.get(importPath);
 	}
 
-	public void init(){
-		keyValueCache = CacheBuilder.newBuilder()
-				.expireAfterWrite(10, TimeUnit.SECONDS)
+	public String getExport() {
+		return export;
+	}
+
+	public void setExport(String export) {
+		this.export = export;
+	}
+
+	public String getExportHour() {
+		return exportHour;
+	}
+
+	public void setExportHour(String exportHour) {
+		this.exportHour = exportHour;
+	}
+
+	public String getExportWeek() {
+		return exportWeek;
+	}
+
+	public void setExportWeek(String exportWeek) {
+		this.exportWeek = exportWeek;
+	}
+
+	public void init() {
+		keyValueCache = CacheBuilder.newBuilder().expireAfterWrite(10, TimeUnit.SECONDS)
 				.build(new CacheLoader<Integer, String>() {
 					@Override
 					public String load(Integer id) throws Exception {
 						System.out.println("***执行方法走内存***");
-						return id+"#"+"---"+importPath.toString();
+						return id + "#" + "---" + importPath.toString();
 					}
-				});
+		});
+		
+		String[] dirs = new String[]{this.getExport(),this.getExportHour(),this.getExportWeek()};
+		for (String dir : dirs) {
+			Path path = Paths.get(dir);
+			try {
+				Files.createDirectories(path);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	
-	public String getValue(Integer id){
+
+	public String getValue(Integer id) {
 		String result = null;
 		try {
 			result = keyValueCache.get(id);
@@ -39,4 +77,5 @@ public class LocalCacheImpl {
 		}
 		return result;
 	}
+	
 }
